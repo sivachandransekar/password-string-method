3aefa5d49d08b66f49710b468e702c4a80e3e6c6
siva <sivasebi4545@gmail.com>
2023-07-28 16:39:30 +0530

    password

---
+++ main.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter the password");
+
+document.write("Given Password = "+a+"<br><br>");
+
+var b=[];
+
+for (i=0; i<a.length;i++){
+	if (a[i]==" "){
+		document.write(alert("this password is incorrect"));
+	}
+}
+document.write(b);
